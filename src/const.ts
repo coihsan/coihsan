1c86353616a554e618e37f5695968d19d6d9e13d
coihsan <ihsan.onlinefs@gmail.com>
2024-03-13 02:18:25 +0700

    error

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,4 @@
+
 export const navlink = [
     { key: 0, href: "/", text: "Home" },
     { key: 1, href: "/source", text: "Source" },
@@ -6,32 +7,32 @@ export const navlink = [
     { key: 3, href: "/grandpa", text: "YourGrandpa" },
   ];
   export const sociallink = [
-    {key: 0, icon: "/src/assets/x-twitter.svg", href: "#", alt: "twitter"},
-    {key: 1, icon: "/src/assets/instagram.svg", href: "#", alt: "instagram"},
-    {key: 2, icon: "/src/assets/github.svg", href: "#", alt: "github"},
-    {key: 3, icon: "/src/assets/codepen.svg", href: "#", alt: "codepen"},
+    {key: 0, icon: "/assets/x-twitter.svg", href: "#", alt: "twitter"},
+    {key: 1, icon: "/assets/instagram.svg", href: "#", alt: "instagram"},
+    {key: 2, icon: "/assets/github.svg", href: "#", alt: "github"},
+    {key: 3, icon: "/assets/codepen.svg", href: "#", alt: "codepen"},
   ]
   export const nextjs = ["Landing Page", "Blog", "Portfolios", "Company Profile"];
   export const techstack = [
-    { key: 0, title: "HTML 5", icon: "/src/assets/html.svg", href: "https://html.com/html5/" },
-    { key: 1, title: "CSS 3", icon: "/src/assets/css.svg", href: "#" },
-    { key: 2, title: "JavaScript", icon: "/src/assets/js.svg", href: "https://www.javascript.com/" },
-    { key: 3, title: "TypeScript", icon: "/src/assets/ts.svg", href: "https://www.typescriptlang.org/" },
-    { key: 4, title: "React.js", icon: "/src/assets/react.svg", href: "https://react.dev/" },
-    { key: 5, title: "Tailwind CSS", icon: "/src/assets/tailwind.svg", href: "https://tailwindcss.com/" },
-    { key: 6, title: "Next.js", icon: "/src/assets/nextjs.svg", href: "https://nextjs.org/" },
-    { key: 7, title: "GitHub", icon: "/src/assets/github.svg", href: "https://github.com/" },
-    { key: 8, title: "Node.js", icon: "/src/assets/nodejs.svg", href: "https://nodejs.org/en" },
-    { key: 9, title: "Redux", icon: "/src/assets/redux.svg", href: "https://redux.js.org/" },
-    { key: 10, title: "Jest", icon: "/src/assets/jest.svg", href: "https://jestjs.io/" },
-    { key: 11, title: "MongoDB", icon: "/src/assets/mongodb.svg", href: "https://www.mongodb.com/developer/" },
+    { key: 0, title: "HTML 5", icon: "/assets/html.svg", href: "https://html.com/html5/" },
+    { key: 1, title: "CSS 3", icon: "/assets/css.svg", href: "#" },
+    { key: 2, title: "JavaScript", icon: "/assets/js.svg", href: "https://www.javascript.com/" },
+    { key: 3, title: "TypeScript", icon: "/assets/ts.svg", href: "https://www.typescriptlang.org/" },
+    { key: 4, title: "React.js", icon: "/assets/react.svg", href: "https://react.dev/" },
+    { key: 5, title: "Tailwind CSS", icon: "/assets/tailwind.svg", href: "https://tailwindcss.com/" },
+    { key: 6, title: "Next.js", icon: "/assets/nextjs.svg", href: "https://nextjs.org/" },
+    { key: 7, title: "GitHub", icon: "/assets/github.svg", href: "https://github.com/" },
+    { key: 8, title: "Node.js", icon: "/assets/nodejs.svg", href: "https://nodejs.org/en" },
+    { key: 9, title: "Redux", icon: "/assets/redux.svg", href: "https://redux.js.org/" },
+    { key: 10, title: "Jest", icon: "/assets/jest.svg", href: "https://jestjs.io/" },
+    { key: 11, title: "MongoDB", icon: "/assets/mongodb.svg", href: "https://www.mongodb.com/developer/" },
   ];
   export const otherstack =[
-    { key: 0, title: "Google Ads", icon: "/src/assets/googleAds.svg", href: "https://ads.google.com/" },
-    { key: 1, title: "SEMRush", icon: "/src/assets/semrush.svg", href: "https://www.semrush.com/" },
-    { key: 1, title: "Search Console", icon: "/src/assets/search-console.svg", href: "https://search.google.com/" },
-    { key: 1, title: "Google Analytics", icon: "/src/assets/semrush.svg", href: "https://analytics.google.com/" },
-    { key: 1, title: "Tag Manager", icon: "/src/assets/tagmanager.svg", href: "https://tagmanager.google.com/" },
+    { key: 0, title: "Google Ads", icon: "/assets/googleAds.svg", href: "https://ads.google.com/" },
+    { key: 1, title: "SEMRush", icon: "/assets/semrush.svg", href: "https://www.semrush.com/" },
+    { key: 1, title: "Search Console", icon: "/assets/search-console.svg", href: "https://search.google.com/" },
+    { key: 1, title: "Google Analytics", icon: "/assets/semrush.svg", href: "https://analytics.google.com/" },
+    { key: 1, title: "Tag Manager", icon: "/assets/tagmanager.svg", href: "https://tagmanager.google.com/" },
   ]
   export const projects = [
     {
