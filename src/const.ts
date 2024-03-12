126c6c4a73f3789c4afd5cbcb1f809dbfda0c26d
coihsan <ihsan.onlinefs@gmail.com>
2024-03-12 13:48:17 +0700

    msg

---
+++ src/const.ts
@@ -0,0 +1,46 @@
+export const navlink = [
+    { key: 0, href: "/", text: "Home" },
+    { key: 1, href: "#", text: "Source" },
+    { key: 2, href: "/stack", text: "Stack" },
+    { key: 3, href: "/contact", text: "Contact" },
+  ];
+  export const sociallink = [
+    {key: 0, icon: "/x-twitter.svg", href: "#", alt: "twitter"},
+    {key: 1, icon: "/instagram.svg", href: "#", alt: "instagram"},
+    {key: 2, icon: "/github.svg", href: "#", alt: "github"},
+    {key: 3, icon: "/codepen.svg", href: "#", alt: "codepen"},
+  ]
+  export const nextjs = ["Landing Page", "Blog", "Portfolios", "Company Profile"];
+  export const techstack = [
+    { key: 0, title: "HTML 5", icon: "/html.svg", href: "https://html.com/html5/" },
+    { key: 1, title: "CSS 3", icon: "/css.svg", href: "#" },
+    { key: 2, title: "JavaScript", icon: "/js.svg", href: "https://www.javascript.com/" },
+    { key: 3, title: "TypeScript", icon: "/ts.svg", href: "https://www.typescriptlang.org/" },
+    { key: 4, title: "React.js", icon: "/react.svg", href: "https://react.dev/" },
+    { key: 5, title: "Tailwind CSS", icon: "/tailwind.svg", href: "https://tailwindcss.com/" },
+    { key: 6, title: "Next.js", icon: "/nextjs.svg", href: "https://nextjs.org/" },
+    { key: 7, title: "GitHub", icon: "/github.svg", href: "https://github.com/" },
+    { key: 8, title: "Node.js", icon: "/nodejs.svg", href: "https://nodejs.org/en" },
+    { key: 9, title: "Redux", icon: "/redux.svg", href: "https://redux.js.org/" },
+    { key: 10, title: "Jest", icon: "/jest.svg", href: "https://jestjs.io/" },
+    { key: 11, title: "MongoDB", icon: "/mongodb.svg", href: "https://www.mongodb.com/developer/" },
+  ];
+  export const otherstack =[
+    { key: 0, title: "Google Ads", icon: "/googleAds.svg", href: "https://ads.google.com/" },
+    { key: 1, title: "SEMRush", icon: "/semrush.svg", href: "https://www.semrush.com/" },
+    { key: 1, title: "Search Console", icon: "/search-console.svg", href: "https://search.google.com/" },
+    { key: 1, title: "Google Analytics", icon: "/semrush.svg", href: "https://analytics.google.com/" },
+    { key: 1, title: "Tag Manager", icon: "/tagmanager.svg", href: "https://tagmanager.google.com/" },
+  ]
+  export const projects = [
+    {
+      key: 0,
+      image: "/",
+      title: "Tama App",
+      description:
+        "Free task manager for a personal or team projects such as funnelling, daily task, note documentation, board and more.",
+      useCase: "Web App",
+      linkDemo: "tama.app",
+    },
+  ];
+  
