41eba159138f0a496781c1d4541f63718aa78322
coihsan <ihsan.onlinefs@gmail.com>
2024-03-12 21:10:49 +0700

    edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ export default {
 				".container": {
 					"@apply max-w-[1024px] mx-auto px-5 md:px-10 lg:px-16 xl:max-w-[87.5rem]": {},
 				},
+				".boxShadow": {
+					"@apply shadow-[0_0_7px_2px_rgba(69,107,12,42)]": {},
+				},
+				".dotHover": {
+					"@apply group-hover:w-[2px] group-hover:h-[2px] group-hover:bg-lime-400 transition-all ease-linear duration-500 delay-0 rounded-full group-hover:boxShadow": {},
+				},
 			})
 		}),
 	],
