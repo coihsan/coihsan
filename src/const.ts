c559f6c0629c79d09910593425119d91331acc11
coihsan <ihsan.onlinefs@gmail.com>
2024-03-21 04:50:01 +0700

    update

--- src/const.ts
+++ src/const.ts
@@ -1,62 +1,40 @@
 export const SITE_TITLE = "˗ˏˋIhsanˎˊ";
 export const SITE_DESCRIPTION = 'Frontend developer and digital marketing enthusiast | This is my personal web';
 export const navlink = [
-    { key: 0, href: "/", text: "Home" },
-    { key: 1, href: "/stack", text: "Stack" },
-    { key: 2, href: "/projects", text: "Projects" },
-    { key: 3, href: "/themes", text: "Themes" },
+    { href: "/", text: "Home" },
+    { href: "/stack", text: "Stack" },
+    { href: "/projects", text: "Projects" },
+    { href: "/themes", text: "Themes" },
 ];
   export const sociallink = [
-    {key: 0, icon: "/x-twitter.svg", href: "/", alt: "twitter"},
-    {key: 1, icon: "/instagram.svg", href: "https://www.instagram.com/co.ihsan/", alt: "instagram"},
-    {key: 2, icon: "/github.svg", href: "https://github.com/coihsan", alt: "github"},
-    {key: 3, icon: "/codepen.svg", href: "https://codepen.io/coihsan", alt: "codepen"},
+    {icon: "/x-twitter.svg", href: "/", alt: "twitter"},
+    {icon: "/instagram.svg", href: "https://www.instagram.com/co.ihsan/", alt: "instagram"},
+    {icon: "/github.svg", href: "https://github.com/coihsan", alt: "github"},
+    {icon: "/codepen.svg", href: "https://codepen.io/coihsan", alt: "codepen"},
   ]
   export const nextjs = ["Landing Page", "Blog", "Portfolios", "Company Profile"];
   export const techstack = [
-    { key: 0, title: "HTML 5", icon: "/html.svg", href: "https://html.com/html5/" },
-    { key: 1, title: "CSS 3", icon: "/css.svg", href: "/" },
-    { key: 2, title: "JavaScript", icon: "/js.svg", href: "https://www.javascript.com/" },
-    { key: 3, title: "TypeScript", icon: "/ts.svg", href: "https://www.typescriptlang.org/" },
-    { key: 4, title: "React.js", icon: "/react.svg", href: "https://react.dev/" },
-    { key: 5, title: "Tailwind CSS", icon: "/tailwind.svg", href: "https://tailwindcss.com/" },
-    { key: 6, title: "Next.js", icon: "/nextjs.svg", href: "https://nextjs.org/" },
-    { key: 7, title: "GitHub", icon: "/github.svg", href: "https://github.com/" },
-    { key: 8, title: "Node.js", icon: "/nodejs.svg", href: "https://nodejs.org/en" },
-    { key: 9, title: "Redux", icon: "/redux.svg", href: "https://redux.js.org/" },
-    { key: 10, title: "Jest", icon: "/jest.svg", href: "https://jestjs.io/" },
-    { key: 11, title: "MongoDB", icon: "/mongodb.svg", href: "https://www.mongodb.com/developer/" },
+    { title: "HTML 5", icon: "/html.svg", href: "https://html.com/html5/" },
+    { title: "CSS 3", icon: "/css.svg", href: "/" },
+    { title: "JavaScript", icon: "/js.svg", href: "https://www.javascript.com/" },
+    { title: "TypeScript", icon: "/ts.svg", href: "https://www.typescriptlang.org/" },
+    { title: "React.js", icon: "/react.svg", href: "https://react.dev/" },
+    { title: "Tailwind CSS", icon: "/tailwind.svg", href: "https://tailwindcss.com/" },
+    { title: "Next.js", icon: "/nextjs.svg", href: "https://nextjs.org/" },
+    { title: "GitHub", icon: "/github.svg", href: "https://github.com/" },
+    { title: "Node.js", icon: "/nodejs.svg", href: "https://nodejs.org/en" },
+    { title: "Redux", icon: "/redux.svg", href: "https://redux.js.org/" },
+    { title: "Jest", icon: "/jest.svg", href: "https://jestjs.io/" },
+    { title: "MongoDB", icon: "/mongodb.svg", href: "https://www.mongodb.com/developer/" },
+    { title: "Figma", icon: "/figma.svg", href: "https://www.mongodb.com/developer/" },
   ];
   export const otherstack =[
-    { key: 0, title: "Google Ads", icon: "/googleAds.svg", href: "https://ads.google.com/" },
-    { key: 1, title: "SEMRush", icon: "/semrush.svg", href: "https://www.semrush.com/" },
-    { key: 1, title: "Search Console", icon: "/search-console.svg", href: "https://search.google.com/" },
-    { key: 1, title: "Google Analytics", icon: "/analytics.svg", href: "https://analytics.google.com/" },
-    { key: 1, title: "Tag Manager", icon: "/tagmanager.svg", href: "https://tagmanager.google.com/" },
+    { title: "Gemini", icon: "/gemini.svg", href: "https://gemini.google.com/app" },
+    { title: "ChatGPT", icon: "/chatgpt.svg", href: "https://chat.openai.com/" },
+    { title: "Google Ads", icon: "/googleAds.svg", href: "https://ads.google.com/" },
+    { title: "SEMRush", icon: "/semrush.svg", href: "https://www.semrush.com/" },
+    { title: "Search Console", icon: "/search-console.svg", href: "https://search.google.com/" },
+    { title: "Google Analytics", icon: "/analytics.svg", href: "https://analytics.google.com/" },
+    { title: "Tag Manager", icon: "/tagmanager.svg", href: "https://tagmanager.google.com/" },
   ]
-  export const projects = [
-    {
-      image: "/tiditodo.webp",
-      logo: "/",
-      title: "tiditodo App",
-      description:
-        "Free task manager for a personal or team projects such as funnelling, daily task, note documentation, board and more.",
-      useCase: "Web App",
-      techstack: "Next.js",
-      linkDemo: "https://tiditodo.app/",
-      pathname: "tiditodo.app",
-      pathURL: "/tiditodo.md",
-    },
-    {
-      image: "/tiditodo.webp",
-      logo: "/",
-      title: "MangoLabs Agency",
-      description:
-        "Free task manager for a personal or team projects such as funnelling, daily task, note documentation, board and more.",
-      useCase: "Web App",
-      linkDemo: "/mangolabs.app",
-      pathname: "mangolabs.app",
-      pathURL: "/mangolabs.md",
-    },
-  ];
   
