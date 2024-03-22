6e3276771bf4f37d07ce8a2940e3e620b8dbab47
coihsan <ihsan.onlinefs@gmail.com>
2024-03-22 20:22:20 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://coihsan.vercel.app/",
-  integrations: [
-    tailwind({
-      applyBaseStyles: true,
-      nesting: true,
-    })
-  ]
+  integrations: [tailwind({
+    applyBaseStyles: true,
+    nesting: true
+  }), preact({ compat: true })]
 });
