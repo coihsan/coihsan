34990134578a61d9d214d7b588d86ad1be52466a
coihsan <ihsan.onlinefs@gmail.com>
2024-03-22 22:40:01 +0700

    add dep

---
+++ src/components/ui/ButtonCopy.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'preact/hooks';
+
+export default function ButtonCopy() {
+    const [isCopied, setIsCopied] = useState(false);
+  
+    const copyEmail = "co.ihsan@gmail.com";
+  
+    const handleCopy = () => {
+      navigator.clipboard.writeText(copyEmail).then(() => {
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1000);
+      });
+    };
+    return (
+      <button
+        type="button"
+        onClick={handleCopy}
+        style={{ color: isCopied ? "#d4d4d8" : "#fafafa" }}
+        className="bg-zinc-950 border border-zinc-900 hover:border-zinc-800 flex items-center justify-center gap-3 px-4 py-3 rounded-md hover:bg-zinc-900 transitionAll"
+      >
+        <img src="/copy-alt.svg" alt="copy alt" />
+        <span className="font-semibold">{isCopied ? "Copied" : "E-Mail"}</span>
+      </button>
+    );
+  }
