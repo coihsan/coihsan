94ef3480888e02419b9b8a53ab776db77c7bbfa6
coihsan <ihsan.onlinefs@gmail.com>
2024-03-25 06:06:40 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://coihsan.vercel.app/",
-  integrations: [
-    tailwind({
+  integrations: [tailwind({
     applyBaseStyles: true,
     nesting: true
-  }), 
-  preact(),
-]
+  }), preact()],
+  output: "static",
+  adapter: vercel()
 });
