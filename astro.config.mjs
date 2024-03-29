3cb71c53909b38f414b7c725ad0405cc58a62aef
coihsan <ihsan.onlinefs@gmail.com>
2024-03-30 06:33:47 +0700

    debuggin again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import vercel from "@astrojs/vercel/static";
+
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  // vite: {
-  //   ssr: {
-  //     external: ["github-slugger"]
-  //     }
-  //  },
   site: "https://coihsan.vercel.app/",
   image: {
     service: passthroughImageService(),
   },
-  integrations: [tailwind({
-    applyBaseStyles: true,
-    nesting: true
-  }), preact(), icon({iconDir: "src/assets/icons",})],
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+      nesting: true
+    }),
+  preact(), 
+  icon({
+    iconDir: "src/assets/icons",
+  }),
+],
   output: "static",
   adapter: vercel()
 });
