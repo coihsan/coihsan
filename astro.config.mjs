c6df373239dddea289f898feb67806a433ea3f21
coihsan <ihsan.onlinefs@gmail.com>
2024-03-29 07:50:30 +0700

    many update and add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/static";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://coihsan.vercel.app/",
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [tailwind({
     applyBaseStyles: true,
     nesting: true
-  }), preact()],
+  }), preact(), icon({iconDir: "src/assets/icons",})],
   output: "static",
   adapter: vercel()
 });
