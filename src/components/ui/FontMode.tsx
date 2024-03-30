43cad0c51ae1a25bb8b13c2fc942f358dac9d93e
coihsan <ihsan.onlinefs@gmail.com>
2024-03-30 19:00:40 +0700

    add pages

---
+++ src/components/ui/FontMode.tsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from "preact/hooks"
+
+export default function FontMode(){
+    const [isBoring, setIsBoring] = useState(true) 
+    const [isEighties, setIsEighties] = useState(false)
+
+    const handleClick(){
+        setIsBoring(isBoring = false);
+    }
+
+    return(
+        <div>
+            <button type="button">Boring Mode!</button>
+            <button type="button">Eighties Mode!</button>
+        </div>
+    )
+}
