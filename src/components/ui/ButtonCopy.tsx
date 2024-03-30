890ad02d8def38b6925d7c62e87f9c43f05bae9e
M.Ihsan <110887888+coihsan@users.noreply.github.com>
2024-03-30 08:14:38 +0700

    ButtonCopy.tsx

--- src/components/ui/ButtonCopy.tsx
+++ src/components/ui/ButtonCopy.tsx
@@ -21,7 +21,7 @@ export default function ButtonCopy() {
         style={{ color: isCopied ? "#d4d4d8" : "#fafafa" }}
         className="bg-zinc-950 border border-zinc-900 hover:border-zinc-800 flex items-center justify-center gap-3 px-4 py-3 rounded-md hover:bg-zinc-900 transitionAll"
       >
-        <img src="/src/assets/icons/copy-alt.svg" alt="copy alt" width={24} height={24} />
+        <img src="/assets/icons/copy-alt.svg" alt="copy alt" width={24} height={24} />
         <span className="font-semibold">{isCopied ? "Copied" : "E-Mail"}</span>
       </button>
     );
