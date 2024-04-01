83f8425d2bc80de51854c11c68a752a6b79251b3
coihsan <ihsan.onlinefs@gmail.com>
2024-04-02 05:28:47 +0700

    ngantuk

--- src/components/preact/FontMode.tsx
+++ src/components/preact/FontMode.tsx
@@ -1,15 +1,24 @@
-import {useState, useEffect} from "preact/hooks"
+import React, {useState, useEffect} from "preact/hooks"
 
-export default function FontMode(){
-    const [isBoring, setIsBoring] = useState(true) 
-    const [isEighties, setIsEighties] = useState(false)
+export const BoringMode = () => {
+    const [boring, setIsBoring] = useState('global.css');
+    const handleThemeChange = (theme) => {
+        setActiveTheme(theme);
+      };
+      useEffect(() => {
+        const linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = `src/styles/${boring}`;
+        document.head.appendChild(linkElement);
+      }, [boring]);
+            
 
-    return(
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full gap-3">
-            <button type="button" 
-            className="bg-white text-black rounded-md font-bold flex items-center justify-center gap-3 px-4 py-3">Boring Mode!</button>
-            <button type="button" 
-            className="bg-white text-black rounded-md flex items-center justify-center gap-3 px-4 py-3 pressNormal">Eighties Mode!</button>
-        </div>
-    )
+    return
+        <button type="button" onClick={() => handleThemeChange('global.css')}
+        className="bg-white text-black rounded-md font-bold flex items-center justify-center gap-3 px-4 py-3">Boring Mode!</button>
+}
+export const EghtiesMode = () =>{
+    return 
+        <button type="button" onClick={() => handleThemeChange('eighties.css')}
+        className="bg-white text-black rounded-md flex items-center justify-center gap-3 px-4 py-3 pressNormal">Eighties Mode!</button>
 }
