e90680a714a448d355937ee8bf003870b49cee9d
coihsan <ihsan.onlinefs@gmail.com>
2024-04-01 21:35:55 +0700

    redesign

--- src/components/preact/ButtonCopy.tsx
+++ src/components/preact/ButtonCopy.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'preact/hooks';
-export default function ButtonCopy() {
+import { useState } from "preact/hooks";
+export default function ButtonCopy({ copyText = "co.ihsan@gmail.com" }) {
     const [isCopied, setIsCopied] = useState(false);
-  
-    const copyEmail = "co.ihsan@gmail.com";
 
-    const handleClick = () => {
-      navigator.clipboard.writeText(copyEmail).then(() =>{
+    const handleCopy = async () => {
+      try {
+        await navigator.clipboard.writeText(copyText);
         setIsCopied(true);
-        setTimeout(() =>{
-          setIsCopied(false);
-        }, 2000)
-      })
-    }
+        setTimeout(() => setIsCopied(false), 1000);
+      } catch (error) {
+        console.error("Error copying email:", error);
+      }
+    };
+  
     return (
       <button
         type="button"
-        onClick={(handleClick) => console.log("click")}
-        style={{ color: isCopied ? "#d4d4d8" : "#fafafa" }}
+        onClick={handleCopy}
+        style={{ color: isCopied ? "#2dd4bf" : "#fafafa" }}
         className="bg-zinc-950 border border-zinc-900 hover:border-zinc-800 flex items-center justify-center gap-3 px-4 py-3 rounded-md hover:bg-zinc-900 transitionAll"
       >
         <img src="src/assets/icons/copy-alt.svg" alt="copy alt" width={24} height={24} />
-        <span className="font-semibold">{isCopied ? "Copied" : "E-Mail"}</span>
+        <span className="font-semibold">{isCopied ? "Copied!" : "E-Mail"}</span>
       </button>
     );
   }
--- src/components/preact/FontMode.tsx
+++ src/components/preact/FontMode.tsx
@@ -4,14 +4,12 @@ export default function FontMode(){
     const [isBoring, setIsBoring] = useState(true) 
     const [isEighties, setIsEighties] = useState(false)
 
-    const handleClick(){
-        setIsBoring(false)
-    }
-
     return(
-        <div>
-            <button type="button">Boring Mode!</button>
-            <button type="button">Eighties Mode!</button>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full gap-3">
+            <button type="button" 
+            className="bg-white text-black rounded-md font-bold flex items-center justify-center gap-3 px-4 py-3">Boring Mode!</button>
+            <button type="button" 
+            className="bg-white text-black rounded-md flex items-center justify-center gap-3 px-4 py-3 pressNormal">Eighties Mode!</button>
         </div>
     )
 }
